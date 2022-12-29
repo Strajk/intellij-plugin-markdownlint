7816507be81852c0c75de0141b51fc5ea4feef7e
GitHub Action <action@github.com>
2022-12-29 09:54:20 +0000

    Template cleanup

--- src/main/kotlin/com/github/strajk/intellijpluginmarkdownlint/listeners/MyProjectManagerListener.kt
+++ src/main/kotlin/com/github/strajk/intellijpluginmarkdownlint/listeners/MyProjectManagerListener.kt
@@ -1,9 +1,9 @@
-package org.jetbrains.plugins.template.listeners
+package com.github.strajk.intellijpluginmarkdownlint.listeners
 
 import com.intellij.openapi.components.service
 import com.intellij.openapi.project.Project
 import com.intellij.openapi.project.ProjectManagerListener
-import org.jetbrains.plugins.template.services.MyProjectService
+import com.github.strajk.intellijpluginmarkdownlint.services.MyProjectService
 
 internal class MyProjectManagerListener : ProjectManagerListener {
 
--- src/main/kotlin/com/github/strajk/intellijpluginmarkdownlint/services/MyApplicationService.kt
+++ src/main/kotlin/com/github/strajk/intellijpluginmarkdownlint/services/MyApplicationService.kt
@@ -1,6 +1,6 @@
-package org.jetbrains.plugins.template.services
+package com.github.strajk.intellijpluginmarkdownlint.services
 
-import org.jetbrains.plugins.template.MyBundle
+import com.github.strajk.intellijpluginmarkdownlint.MyBundle
 
 class MyApplicationService {
 
